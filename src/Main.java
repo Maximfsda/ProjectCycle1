f792aabe2a40d9bc451073ffabbd79c901902bc6
Maximfsda <lisitsin.maxi@yandex.ru>
2022-09-22 23:29:45 +1000

    Homework1(cycle)

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+        //task2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        //task3
+        for (int i = 0; i < 17; i++) {
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+        //task4
+        for (int i = 10; i >= -10 ; i--) {
+                System.out.println(i);
+            }
+        }
+}
